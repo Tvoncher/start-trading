8e0fc5fa3e94c387a36e2abac36248d77ace0153
Tvoncher <tvincher@mail.ru>
2022-12-04 14:38:50 +0000

    added additional blocks

--- src/pages/page-2.js
+++ src/pages/page-2.js
@@ -6,9 +6,21 @@ import Seo from "../components/seo"
 
 const SecondPage = () => (
   <Layout>
-    <h1>Hi from the second page</h1>
-    <p>Welcome to page 2</p>
-    <Link to="/">Go back to the homepage</Link>
+    <br />
+    <br />
+    <br />
+
+    <center>
+      <h1>This page is currently under construction</h1>
+      <img
+        style={{ width: "50%" }}
+        src="https://svgsilh.com/svg_v2/150275.svg"
+        alt="construction"
+      />
+      <h2>
+        We are working day n night to bring you the best service ever seen
+      </h2>
+    </center>
   </Layout>
 )
 
