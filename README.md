9f7ad39a5255f8a872edfb33d3d1311983b4c09e
Tvoncher <tvincher@mail.ru>
2022-12-15 19:04:08 +0000

    improved

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
 ## Just simple site about crypto trading
 
-It's a learning project,so don't expect much. Also its my very first project made with Gatsby. So there is high possibility that I'll collect all possible mistakes developing it.
+Simple site for trading broker. Also its my very first project made with Gatsby. So there is high possibility that I'll collect all possible mistakes developing it.
 Please consider this before reviewing :)
--- gatsby-config.js
+++ gatsby-config.js
@@ -10,9 +10,9 @@
 module.exports = {
   siteMetadata: {
     title: `Start trading`,
-    description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
-    author: `@gatsbyjs`,
-    siteUrl: `https://gatsbystarterdefaultsource.gatsbyjs.io/`,
+    description: `Trading platform`,
+    author: `Tvoncher`,
+    siteUrl: `https://start-trading.web.app/`,
   },
   plugins: [
     `gatsby-plugin-image`,
@@ -32,11 +32,8 @@ module.exports = {
         short_name: `starter`,
         start_url: `/`,
         background_color: `#663399`,
-        // This will impact how browsers show your PWA/website
-        // https://css-tricks.com/meta-theme-color-and-trickery/
-        // theme_color: `#663399`,
         display: `minimal-ui`,
-        icon: `src/images/icon.png`, // This path is relative to the root of the site.
+        icon: `src/images/icon.png`,
       },
     },
   ],
